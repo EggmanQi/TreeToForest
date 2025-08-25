73b74d66c06eb7aac88c1b18387128f60ca45c96
edwinQQQ <exzero@126.com>
2025-08-25 15:16:54 +0800

    Initial Commit

---
+++ TreeToForest/TreeToForestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TreeToForestApp.swift
+//  TreeToForest
+//
+//  Created by P on 2025/8/25.
+//
+
+import SwiftUI
+
+@main
+struct TreeToForestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
